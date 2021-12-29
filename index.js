6db8c4cec60b809a667d8339923962dcd591c887
salih.celik <salih.celik@roofstacks.com>
2021-12-29 20:14:29 +0300

    posts and user conroller complate

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routers/users");
 const authRoute = require("./routers/auth");
+const postRoute = require("./routers/posts");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
-    console.log("Connected to MongoDB");
-});
+mongoose.connect(
+    process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
 
 //middleware
 app.use(express.json());
@@ -20,6 +24,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running !");
